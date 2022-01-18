3808671929e298b2304ae4c0063f9a3a9affdf9c
Trung Phan <miyucoder@gmail.com>
2022-01-18 09:36:06 +0700

    Update WebConfig.java

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -6,7 +6,7 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
