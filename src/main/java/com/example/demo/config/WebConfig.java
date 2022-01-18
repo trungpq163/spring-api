1de769d9c07735df603135fe8b9a78158669cc79
Trung Phan <miyucoder@gmail.com>
2022-01-18 09:35:49 +0700

    Create WebConfig.java

---
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,13 @@
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
+}
