b992176dc98753618296ded67ee2f2e41703a29d
trungpq163 <miyucoder@gmail.com>
2022-01-21 08:12:45 +0700

    add cors

--- src/main/java/com/example/demo/controller/EvaluationController.java
+++ src/main/java/com/example/demo/controller/EvaluationController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/v1")
 public class EvaluationController {
